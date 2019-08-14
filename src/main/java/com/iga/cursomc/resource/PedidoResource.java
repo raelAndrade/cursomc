c106b2eddbdc2075062a2a3adaca62d6bc045700
Israel <israelg.andrade@bol.com.br>
2019-08-14 01:02:08 -0300

    Adicionando Ferramenta de documentação - Swagger

--- src/main/java/com/iga/cursomc/resource/PedidoResource.java
+++ src/main/java/com/iga/cursomc/resource/PedidoResource.java
@@ -1,4 +1,4 @@
-package com.iga.cursomc.resources;
+package com.iga.cursomc.resource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
